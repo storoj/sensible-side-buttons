763ed0faa5d6bac3d83d5871475d8924a9a5ffc2
Alexey Storozhev <dark.storoj@gmail.com>
2023-06-09 13:21:38 +0100

    Delete storyboard

--- SideButtonFixer/main.m
+++ SideButtonFixer/main.m
@@ -20,7 +20,12 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "AppDelegate.h"
 
 int main(int argc, const char * argv[]) {
+    static id<NSApplicationDelegate> delegate;
+    delegate = [AppDelegate new];
+
+    [NSApplication sharedApplication].delegate = delegate;
     return NSApplicationMain(argc, argv);
 }
